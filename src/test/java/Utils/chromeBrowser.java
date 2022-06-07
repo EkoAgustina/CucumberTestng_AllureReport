7264cb229f72f55b8eccfa3d8e075eb077d4f4a9
Eko Agustina <eko.agustina86@gmail.com>
2022-06-07 21:40:53 +0700

    InitUpdate

--- src/test/java/Utils/chromeBrowser.java
+++ src/test/java/Utils/chromeBrowser.java
@@ -2,18 +2,23 @@ package Utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import java.io.File;
 
 public class chromeBrowser {
 
+
     public static WebDriver driver = new ChromeDriver();
+    @Test
 
     public static void PathChrome(){
         File filePath = new File(System.getProperty("user.dir"));
         File chromeExe = new File(filePath, "/chromedirver.exe");
 
         System.getProperty("webdriver-chrome-driver", chromeExe.getAbsolutePath());
+
+
     }
 
 }
